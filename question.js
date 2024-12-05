1bf6163559db5e08a7e4d9c67c159cf1fe7c8906
Kshitij Agnihotri <kshitijagnihotri@Kshitijs-Air.local>
2024-12-05 17:16:35 +0530

    Initial Commit

---
+++ question.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+        numb: 1,
+        question: "How many ports a computer may have ?",
+        answer: "B. 65535",
+        options: [
+            "A. 53556",
+            "B. 65535",
+            "C. 65553",
+            "D. 55635"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The non-ASCII characters would be replaced with ________ by the process of URL encoding ?",
+        answer: "B. ' % ' ",
+        options: [
+            "A. ' + ' ",
+            "B. ' % ' ",
+            "C. ' & ' ",
+            "D. ' * ' "
+        ]
+    },
+    {
+        numb: 3,
+        question: "WWHich of these classes helps make a text stand out?",
+        answer: "B. .lead",  // Removed single quotes
+        options: [
+                    "A. .small",
+                    "B. .lead",
+                    "C. .text-center",
+                    "D. .text-left"
+                ]
+        
+    },
+    {
+        numb: 4,
+        question: "Which of the following CSS properties specifies the stack order of elements?",
+        answer: "A. Z-index ",
+        options: [
+            "A. Z-index",
+            "B. Overlap",
+            "C. Nth child",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of the following are types of length units in CSS?",
+        answer: "C. Both a and b ",
+        options: [
+            "A. Absolute",
+            "B. relative",
+            "C. Both a and b",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which of the following is the correct way to select all h1 headers in a div element?",
+        answer: "A. Div h1 ",
+        options: [
+            "A. Div h1",
+            "B. Div-h1",
+            "C. h1",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which programming language is known as the 'language of the web'?",
+        answer: "A. JavaScript",
+        options: [
+            "A. JavaScript",
+            "B. Python",
+            "C. Java",
+            "D. C++"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Javascript is an _______ language?",
+        answer: "A. Object oriented",
+        options: [
+            "A. Object oriented",
+            "B. Object-based.",
+            "C. Procedural",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Which of the following keywords is usded to define a variable in JavaScript?",
+        answer: "A. Var ",
+        options: [
+            "A. Var",
+            "B. Let",
+            "C. Both a and b",
+            "D. None of the above"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Inventor of www (World wide web) is...",
+        answer: "C. Tim Berners Lee",
+        options: [
+            "A. Lee. N. Feyong",
+            "B. Bill Gates",
+            "C. Tim Berners Lee",
+            "D. Tom Berners Lee"
+        ]
+    }
+];
